1bf1d8e847c328f68ca8675f67c0c7ec9bf98898
PPeter757 <pierre.peterson757@gmail.com>
2025-12-12 12:20:48 -0500

    OTP + Login corrigés

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,19 @@
+<?php
+// Connexion PostgreSQL Render
 $host = "dpg-d4th8rpr0fns73deo5ig-a.oregon-postgres.render.com";
 $dbname = "gestion_boutique";
 $user = "gestion_boutique_user";
 $pass = "p4hMxXFoaL1ZyhaHVtPeTt74nigPD0bS";
 
+// DSN PDO PostgreSQL
 $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
     die("❌ Connexion PostgreSQL échouée : " . $e->getMessage());
 }
+?>
